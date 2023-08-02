86ef503bb3603ee49eec944a5dab63e43f31b9f5
klinsmannAluno <pedrogabriel12gamer@outlook.com>
2023-08-02 16:00:21 -0300

    Cria um componente Gato.

--- App.js
+++ App.js
@@ -5,8 +5,15 @@ const MyApp = () => {
     <>
       <Text>Hello Word</Text>
       <Text>IFAL</Text>
+      <Gato />
+      <Gato />
+      <Gato />
     </>
   );
+};
+
+const Gato = () => {
+  return <Text>Eu sou um Gato.</Text>;
 };
 
 export default MyApp;
